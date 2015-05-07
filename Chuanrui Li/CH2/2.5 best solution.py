6b2cf21329d3763fba2a8b3ece9c98accc43ff47
chuanrui li <richard874410593@gmail.com>
2015-05-07 13:50:09 -0400

    updated best solution

--- Chuanrui Li/CH2/2.5 best solution.py
+++ Chuanrui Li/CH2/2.5 best solution.py
@@ -1,9 +1,15 @@
-# Given a circular linked list, implement an algorithm which returns node at the begin- ning of the loop.
-# DEFINITION
-# Circular linked list: A (corrupt) linked list in which a node’s next pointer points to an earlier node, so as to make a loop in the linked list.
+# Given a circular linked list, implement an algorithm which returns node at the beginning of the loop.
+
 # EXAMPLE
 # input: A -> B -> C -> D -> E -> C [the same C as earlier]
 # output: C
+
+# 1), assume we have two types of runners, first, slow runner(s1) and fast runner (s2). s1 increment by 1 and s2 increment by 2
+# 2), s2 starts at kth position, which is bigger than 0, s1 starts at 0 position
+# 3), if s1 can catch up s2, then that means the path is circle
+
+# 4), s1 will be set at the starting node, s2 will be set at the previous meeting point
+
 
 class Node(object):
     def __init__(self, data):
@@ -58,38 +64,52 @@ def deleteNode(node):
         node.nextNode = node.nextNode.nextNode
         return
 
+def circle(node):
+    #init state
+    faster = node.nextNode
+    slower = node
+    
+    #iteration
+    while faster != slower:
+        if faster == None or slower == None:
+            print "no circle"
+            return
+        elif faster.nextNode == None:
+            print "no circle"
+            return
+        faster = faster.nextNode.nextNode
+        slower = slower.nextNode
+    #     print faster.data, slower.data
+
+    #find the starting position of the cycle
+    slower = node
+    faster = faster.nextNode
+    while faster != slower:
+        print "hehe"
+        faster = faster.nextNode
+        slower = slower.nextNode
+
+    print faster.data
+
 
 
 #first linked list
-node = Node("0", )
+node = Node("0")
 node1 = Node("1")
 node2 = Node("2")
 node3 = Node("3")
 node4 = Node("4")
-node5 = Node("1")
+node5 = Node("5")
 
 node.nextNode = node1
 node1.nextNode = node2
 node2.nextNode = node3
 node3.nextNode = node4
 node4.nextNode = node5
-node5.nextNode = node2
-
-def circle(node):
-    while(node != None):
-        if(node.traveled == 0):
-            node.traveled = 1
-        else:
-            print "circle detect: " + pre
-            break
-        pre = node.data
-        node = node.nextNode
+node5.nextNode = node1
 
 
 circle(node)
 
-
-
-
-
+#cannot handle one circle case
 
