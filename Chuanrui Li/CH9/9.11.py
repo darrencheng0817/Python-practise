8598d22067c17769f0bce01e227d5939227f0738
lichuanr <richard874410593@gmail.com>
2015-06-10 13:23:59 -0400

    bug fixed

--- Chuanrui Li/CH9/9.11.py
+++ Chuanrui Li/CH9/9.11.py
@@ -10,42 +10,51 @@
 
 def ans(start, end, operations, result):
 	if start >= end-1:
+		print "hehe-------------"
+		print start, end
 		
-		print "hehe"
+		
 		if operations[start] == '1' and result == True:
 			return 1
 		if operations[start] == '0' and result == False:
 			return 1
 			#this is same logic
 		return 0
-
+	#for each, the program need to count all the possiblities, not only one satified and then return
+	counter = 0
 	if result == True:
 		i = start
 		while i < end:
+			print "in loop"
+			print "index: " + str(i), "result: " + str(operations[i])
 			#find the value for each operation
 			if operations[i] == '^':
-				return ans(start, i-1, operations, True) * ans(i+1, end, operations, False) + ans(start, i-1, operations, False) * ans(i+1, end, operations, True)
+				counter += ans(start, i-1, operations, True) * ans(i+1, end, operations, False) + ans(start, i-1, operations, False) * ans(i+1, end, operations, True)
 			elif operations[i] == '&':
-				return ans(start, i-1, operations, True) * ans(i+1, end, operations, True)
+				counter += ans(start, i-1, operations, True) * ans(i+1, end, operations, True)
 			elif operations[i] == '|':
-				return ans(start, i-1, operations, True) * ans(i+1, end, operations, True) + ans(start, i-1, operations, True) * ans(i+1, end, operations, False) + ans(start, i-1, operations, False) * ans(i+1, end, operations, True)
+				counter += ans(start, i-1, operations, True) * ans(i+1, end, operations, True) + ans(start, i-1, operations, True) * ans(i+1, end, operations, False) + ans(start, i-1, operations, False) * ans(i+1, end, operations, True)
 			i += 1		
 	if result == False:
 		#this is same logic, different true, false conditions
 		i = start
 		while i < end:
+			print "in loop2"
+			print i, operations[i]			
 			#find the value for each operation
 			if operations[i] == '^':
-				return ans(start, i-1, operations, True) * ans(i+1, end, operations, True) + ans(start, i-1, operations, False) * ans(i+1, end, operations, False)
+				counter += ans(start, i-1, operations, True) * ans(i+1, end, operations, True) + ans(start, i-1, operations, False) * ans(i+1, end, operations, False)
 			elif operations[i] == '&':
-				return ans(start, i-1, operations, False) * ans(i+1, end, operations, False) + ans(start, i-1, operations, True) * ans(i+1, end, operations, False) + ans(start, i-1, operations, False) * ans(i+1, end, operations, True)
+				counter += ans(start, i-1, operations, False) * ans(i+1, end, operations, False) + ans(start, i-1, operations, True) * ans(i+1, end, operations, False) + ans(start, i-1, operations, False) * ans(i+1, end, operations, True)
 			elif operations[i] == '|':
-				return ans(start, i-1, operations, False) * ans(i+1, end, operations, False)
+				counter += ans(start, i-1, operations, False) * ans(i+1, end, operations, False)
 			i += 1
+	return counter
 
 
 def main():
-	operations = ['1', '|', '1', '|', '1']
+	# 1 | (1 & 1)
+	operations = ['0', '&', '1', '|', '0']
 	result = True
 	print ans(0, len(operations), operations, result)
   
