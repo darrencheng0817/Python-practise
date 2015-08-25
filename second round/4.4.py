2c9b56bccd397eae8fa092934a33cc87cd9be075
lichuanr <richard874410593@gmail.com>
2015-08-24 22:28:02 -0400

    new code

--- second round/4.4.py
+++ second round/4.4.py
@@ -1,3 +1,26 @@
+#check balance
+class Node:
+	def __init__(self, data, left = None, right = None):
+		self.data = data
+		self.left = left
+		self.right = right
+
+t = Node(1, Node(2, Node(4), Node(4)), Node(3, Node(4)))
+
+def height(node):
+	if node == None:
+		return 0
+	return max(height(node.right), height(node.left)) + 1
+
+
+def balance(node):
+	if node == None:
+		return True
+	else:
+		return balance(node.left) and balance(node.right) and abs(height(node.left) - height(node.right)) < 1
+
+
+print balance(t)
 
 
 
--- second round/4.5.py
+++ second round/4.5.py
@@ -1,6 +1,36 @@
+#validate BST
+#if no duplicates, we can directly use in order walk
 
 
+class Node:
+	def __init__(self, data, left = None, right = None):
+		self.data = data
+		self.left = left
+		self.right = right
 
 
+t = Node(4, Node(2, Node(1), Node(4.4)), Node(9, Node(8), Node(10)))
 
+#tracking the previous variable -> temp buffer
+pre = None
 
+def checkBST(node):
+	global pre
+	if node == None:
+		return True
+	#checking BST
+	if checkBST(node.left) == False:
+		return False
+
+	if pre != None:
+		if pre > node.data:
+			return False
+
+	pre = node.data
+	
+	if checkBST(node.right) == False:
+		return False
+
+	return True
+
+print checkBST(t)
